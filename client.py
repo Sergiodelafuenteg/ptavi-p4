d6e88230ded64232780a033b69dddcdfa6732955
Sergio  De La Fuente Garcia <sfuente@gamma14.aulas.gsyc.urjc.es>
2017-10-23 17:40:13 +0200

    Definido SIP2.0

--- client.py
+++ client.py
@@ -8,21 +8,24 @@ import socket
 import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = sys.argv[1]
-PORT = int(sys.argv[2])
-LINE = sys.argv[3:]
+try:
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    LINE = sys.argv[3:]
 
-METODO = LINE[0].upper()
-ADDRESS = "sip:" + LINE[1]
-PROTOCOL = 'SIP/2.0 \r\n'
-EXPIRES = sys.argv[4] + '\r\n\r\n'
-
+    METODO = LINE[0].upper()
+    ADDRESS = "sip:" + LINE[1]
+    PROTOCOL = 'SIP/2.0 \r\n'
+    EXPIRES = 'Expires: ' + LINE[2] + '\r\n\r\n'
+    DATA = ' '.join([METODO,ADDRESS,PROTOCOL,EXPIRES])
+except IndexError:
+    sys.exit('Usage: client.py ip puerto register sip_address expires_value')
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    print("Enviando:", DATA)
+    my_socket.send(bytes(DATA, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
