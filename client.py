17d35265ca13676e029fd31c2f41010abe96b0ba
Sergio  De La Fuente Garcia <sfuente@gamma14.aulas.gsyc.urjc.es>
2017-10-30 16:06:49 +0100

    arreglado error en el cliente

--- client.py
+++ client.py
@@ -9,18 +9,18 @@ import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
 try:
-    SERVER = sys.argv[1]
-    PORT = int(sys.argv[2])
-    LINE = sys.argv[3:]
+    _, SERVER, PORT, METODO, SIP_ADDRESS, EXPIRES = sys.argv
 
-    METODO = LINE[0].upper()
-    ADDRESS = "sip:" + LINE[1]
-    PROTOCOL = 'SIP/2.0\r\n'
-    EXPIRES = 'Expires: ' + LINE[2] + '\r\n\r\n'
-    DATA = ' '.join([METODO,ADDRESS,PROTOCOL,EXPIRES])
 except IndexError:
     sys.exit('Usage: client.py ip puerto register sip_address expires_value')
 
+PORT = int(PORT)
+METODO = METODO.upper()
+ADDRESS = "sip:" + SIP_ADDRESS
+PROTOCOL = 'SIP/2.0\r\n'
+EXPIRES = 'Expires: ' + EXPIRES + '\r\n\r\n'
+DATA = ' '.join([METODO,ADDRESS,PROTOCOL])
+DATA = DATA + EXPIRES
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
