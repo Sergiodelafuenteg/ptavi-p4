39b7e15aa199d5305e53e65ed2261934d7a752a4
Sergio  De La Fuente Garcia <sfuente@gamma14.aulas.gsyc.urjc.es>
2017-10-30 17:33:22 +0100

    estilo pep8 y pep257

--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Clase (y programa principal) para un servidor de eco en UDP simple
-"""
+"""Clase (y programa principal) para un servidor de eco en UDP simple."""
 
 import socketserver
 import sys
@@ -13,16 +11,17 @@ ATTR_TIME = '%Y-%m-%d %H:%M:%S +0000'
 
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
-    """Echo server class"""
+    """Echo server class."""
+
     Users = {}
 
     def register2json(self):
-        """metodo para registrar usuarios en json"""
+        """metodo para registrar usuarios en json."""
         with open('registered.json', 'w') as outfile:
             json.dump(self.Users, outfile, indent=3)
 
     def check_exp(self, act_time):
-        """Checkear expiracion del user"""
+        """Checkear expiracion del user."""
         list_del = []
         for address in self.Users:
             if self.Users[address]['expire'] <= act_time:
@@ -31,6 +30,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             del self.Users[address]
 
     def json2registered(self):
+        """metodo para leer json externo."""
         try:
             with open('registered.json', 'r') as infile:
                 self.Users = json.loads(infile)
@@ -38,10 +38,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             pass
 
     def handle(self):
-        """
-        handle method of the server class
-        (all requests will be handled by this method)
-        """
+        """handle method of the server class."""
         if not self.Users:
             self.json2registered()
         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
